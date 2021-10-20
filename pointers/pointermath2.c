52f9ff15933b5ad40bc40ff84a45acdcac8595a0
Prajukta Dey <theprajuktadey@gmail.com>
2021-10-20 12:12:24 +0530

    Added a file.

---
+++ pointers/pointermath2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () 
+{
+   int twos[5]= {2, 4, 6, 8, 10};
+   int *pt;//assignment of the pointer//
+
+   pt=twos;
+   printf("%p\n", pt);
+   printf("%p\n", pt+1);
+
+   return 0;
+}
