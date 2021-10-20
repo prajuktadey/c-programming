7508a6b394986449a2f7de6f58666a40b4c61446
Prajukta Dey <theprajuktadey@gmail.com>
2021-10-20 18:30:20 +0530

    Added a file.

---
+++ pointers/arrayp2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () 
+{
+   char *veeps[5]= {"Adams", "Jefferson", "Andy", "Marianne", "Toby"};
+   int x;
+
+   for(x=0;x<5;x++)
+        printf("%s\n", *(veeps+x));//pointer notation//
+   return 0;
+}
