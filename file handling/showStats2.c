7d5fd7a92e6a90cca35d8a15ff0a53e26f6df788
Prajukta Dey <theprajuktadey@gmail.com>
2021-10-22 09:49:48 +0530

    Added a file.

---
+++ file handling/showStats2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <time.h>
+
+int main()
+{
+//stat function reads each file from the current directory//
+	DIR *directory;
+	struct dirent *file;
+	struct stat fstat;
+
+	/* open the directory */
+	directory = opendir(".");
+	if( directory == NULL )
+	{
+		puts("Unable to open current directory");
+		return(1);
+	}
+
+	/* do something */
+	printf("%-25s %-10s %s\n","Filename","Size","Timestamp");
+	while( (file=readdir(directory)) != NULL )//to read information returned by the readdir function//
+	{
+		stat(file->d_name,&fstat);
+		printf("%25s %10lld ",
+				file->d_name,
+				fstat.st_size
+			  );
+		printf("%s",ctime(&fstat.st_mtime));
+	}
+
+	/* close the directory */
+	closedir(directory);
+
+	return(0);
+}
+
