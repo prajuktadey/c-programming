f9b60c97df89688dc44f3524d3c09570c9a09c15
Prajukta Dey <theprajuktadey@gmail.com>
2021-10-22 09:46:42 +0530

    Added a file.

--- file handling/showStats.c
+++ file handling/showStats.c
@@ -7,12 +7,12 @@ int main()
 	const char filename[] = "alpha.txt";
 	struct stat fstat;
 
-	stat(filename,&fstat);
-	printf("%s is %ld bytes long\n",
+	stat(filename,&fstat);//first argument is the file name and the second argument is the address of the stat() structure which is fstat//
+	printf("%s is %ld bytes long\n",//accesses the st_size member which lists the file size//
 			filename,
 			fstat.st_size
 		  );
-	printf("It was modified on %s",ctime(&fstat.st_mtime));
+	printf("It was modified on %s",ctime(&fstat.st_mtime));//accesses the st_mtime member which contains the clock tick value representing when the file was last modified//
 
 	return(0);
 }
