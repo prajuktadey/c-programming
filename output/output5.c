0ab5a90ee5851106a01de63ef3c799481d2beb50
Prajukta Dey <theprajuktadey@gmail.com>
2022-01-03 21:55:10 +0530

    Added a file.

---
+++ output/output5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i = 3;
+    switch (i)
+    {
+        case 0+1: printf("Geeks");
+                break;
+        case 1+2: printf("Quiz");
+                break;
+        default: printf("GeeksQuiz");
+    }
+    return 0;
+}
