d130f01cd056ffd3e15981fa46cd7be39843856c
Prajukta Dey <theprajuktadey@gmail.com>
2022-01-05 17:26:00 +0530

    Added a file.

--- output/output20.c
+++ output/output20.c
@@ -3,11 +3,11 @@
 int main (int argc, char *argv[]) \
 {
 
-                   int m [] = {1,2,3,4,5};
-                   int x,y=0;
-                   for ( x=0; x<5; x++ )
-                             y=y+m [x];
-                   printf("%d", y);
+    int m [] = {1,2,3,4,5};
+    int x,y=0;
+    for ( x=0; x<5; x++ )
+         y=y+m [x];
+    printf("%d", y);
  
 return 0;
 }
