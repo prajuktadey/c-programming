f60265419b3c6c9445b564dbf63c60ee60cc87e8
Prajukta Dey <theprajuktadey@gmail.com>
2022-01-05 12:07:47 +0530

    Added a file.

---
+++ output/output18.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) 
+{ 
+   char str1[4]= "GOOD";
+   char str2[]= "C";
+   char str3[5]= "Moon";
+   char str4[]= {'S', 'U', 'N'};
+   char str5[10]= "Sun";
+
+   printf("%s", str1);
+   printf("%s", str2);
+   printf("%s", str3);
+   printf("%s", str4);
+   printf("%s", str5);
+   return 0;
+}
