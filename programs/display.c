7f0e10c25b2524e26d5bd272af81ca73a6452679
prajuktadey <theprajuktadey@gmail.com>
2022-03-17 20:20:23 +0530

    Added a file.

---
+++ programs/display.c
@@ -0,0 +1,12 @@
+//display it in the following format
+#include <stdio.h>
+
+int main () 
+{
+    printf("SL.NO \t Amount \t Discount\n");
+    printf("1 \t $12.96 \t $1.20\n");
+    printf("2 \t $132.63 \t 20.230\n");
+    printf("3 \t $1792.6 \t 3231.36\n");
+
+    return 0;
+}
